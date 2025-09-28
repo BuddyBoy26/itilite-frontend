7710f07dbb3af5eede6d9ebc6f72d7ba9ef08f70
Anshuman Singh <anshumansinghiitkgp@gmail.com>
2025-09-29 00:48:16 +0530

    Added a video, dynamic counter and an Image text section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import HeroSection from "./components/HeroSection";
 import FeaturesSection from "./components/FeaturesSection";
 import Testimonials from "./components/Testimonials";
 import FeatureCarousel from "./components/FeatureCarousel";
+import YtVideo from "./components/YtVideo";
+import DynamicCounter from "./components/DynamicCounter";
+import ImageTextSection from "./components/ImageTextSection";
 
 const Home = () => {
   return (
@@ -9,7 +12,13 @@ const Home = () => {
       <HeroSection />
       <FeatureCarousel />
       <FeaturesSection />
+      <YtVideo 
+         url="https://www.youtube.com/embed/JiCxfStLSEA?si=C_3QdFwYL9HRbgQn" 
+        title="Empower users to book the next business trip on itilite. Watch how it works!"                  
+      />
       <Testimonials />
+      <DynamicCounter />
+      <ImageTextSection sectionTitle="Make corporate travel management easy and save money" />
     </>
   );
 };
